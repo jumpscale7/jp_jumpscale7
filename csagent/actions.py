862b66e98d99eb32de8ab34e08aa2f7ef9b7ddec
Amr Hassan <amr.hassan@codescalers.com>
2015-01-27 15:29:37 +0200

    Packaging files for csagent

---
+++ csagent/actions.py
@@ -0,0 +1,40 @@
+from JumpScale import j
+
+ActionsBase=j.packages.getActionsBaseClass()
+
+import os.path
+
+
+class Actions(ActionsBase):
+
+
+    def build(self,**args):
+    
+        # JPackage is responsible for pulling the source code and putting it here
+        BUILD_BASE = '/opt/build/github.com/Jumpscale/csagent/'
+
+        LUAROCKS_COMMAND = '/opt/luajit/bin/luarocks --server=http://rocks.moonscript.org/manifests/amrhassan'
+
+        def lua_deps():
+            """Returns an array of Lua module dependencies that need to be retrieved from MoonRocks"""
+            deps = open(os.path.join(BUILD_BASE, 'dependencies.txt')).read().split()
+            deps.remove('lunix')    # Bad package. Uninstallable via LuaRocks
+            return deps
+            
+        def install_lunix():
+            LUNIX_BUILD_BASE = '/opt/build/github.com/wahern/lunix'  # It is put there by jpackage before doing the build 
+            LUA_PREFIX = '/opt/luajit'
+            command = 'cd %(build_base)s ; make all prefix=%(prefix)s ; make install prefix=%(dest_prefix)s' %{'build_base': LUNIX_BUILD_BASE, 'prefix': LUA_PREFIX, 'dest_prefix': os.path.join(BUILD_BASE, 'deps')}
+            j.action.start(retry=1, name="install_lua_dep-lunix", description='', cmds=command, action=None, actionRecover=None, actionArgs={}, errorMessage='', die=True, stdOutput=True, jp=self.jp_instance)
+
+        def install_lua_deps():
+            for dep in lua_deps():
+                install_command = '%(luarocks_command)s install --tree=%(local_tree_path)s %(dep)s' % {
+                    'luarocks_command': LUAROCKS_COMMAND, 
+                    'local_tree_path': os.path.join(BUILD_BASE, 'deps'), 
+                    'dep': dep}
+            
+                j.action.start(retry=1, name="install_lua_dep-" + dep, description='', cmds=install_command, action=None, actionRecover=None, actionArgs={}, errorMessage='', die=True, stdOutput=True, jp=self.jp_instance) 
+
+        install_lunix()
+        install_lua_deps()
