7e378d74d12790d89360a9d4c349164de1d48a88
Mohamed Galal <mohamed@codescalers.com>
2015-01-29 03:10:55 +0000

    add redis socket to git-shell config

--- gitlab/actions.py
+++ gitlab/actions.py
@@ -151,6 +151,8 @@ class Actions(ActionsBase):
         os.system("cd /home/git/gitlab && sudo -u git -H bundle exec rake assets:precompile RAILS_ENV=production")
         os.system("cd /home/git/gitlab-shell && sudo -u git -H git fetch && sudo -u git -H git checkout v`cat /home/git/gitlab/GITLAB_SHELL_VERSION`")
         os.system("sudo -u git -H RAILS_ENV=production bin/background_jobs start")
+        j.do.copyFile('/home/git/gitlab-shell/config.yml', '/home/git/gitlab-shell/config.yml.org')
+        os.system("cd /home/git/gitlab-shell && sed 's/socket:.*/\/opt\/jumpscale7\/var\/redis\/main\/redis.sock/' config.yml.org | tee config.yml")
         j.do.execute('service nginx restart && service gitlab restart')
 
         print('userName: root\npassword: 5iveL!fe')
