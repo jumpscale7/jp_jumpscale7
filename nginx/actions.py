edb4266c5c179e596207636d44cc628f4388b227
Hamdy <hamdy.a.farag@gmail.com>
2015-02-03 14:21:50 +0000

    Nginx package

---
+++ nginx/actions.py
@@ -0,0 +1,33 @@
+from JumpScale import j
+import time
+ActionsBase=j.packages.getActionsBaseClass()
+
+class Actions(ActionsBase):
+    
+    def prepare(self, **kwargs):
+        try:
+            j.system.platform.ubuntu.stopService("nginx")
+        except:
+            pass
+
+        cmd="killall nginx"
+        try:
+            j.system.process.execute(cmd)
+        except:
+            pass
+
+        j.system.platform.ubuntu.remove("nginx")
+        j.system.platform.ubuntu.remove("lua-nginx-memcached")
+        j.system.platform.ubuntu.remove("lua-nginx-redis")
+        j.system.platform.ubuntu.remove("nginx-common")
+        j.system.platform.ubuntu.remove("nginx-extras")
+
+        #hack for sandboxed nginx to start properly
+        j.system.fs.createDir(j.system.fs.joinPaths('/var', 'lib', 'nginx'))
+
+    def stop(self, **kwargs):
+        if not j.system.process.getPidsByPort(80):
+            return
+        j.system.process.execute('./nginx -c /opt/nginx/cfg/nginx.conf -s quit ')
+
+     
