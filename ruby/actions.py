78108fcb99e6fc44315a92e58fe787a6fa33ad76
kristof <kristof@incubaid.com>
2015-02-04 09:54:56 +0400

    new

--- ruby/actions.py
+++ ruby/actions.py
@@ -3,115 +3,40 @@ from JumpScale import j
 ActionsBase=j.packages.getActionsBaseClass()
 
 class Actions(ActionsBase):
-    """
-    process for install
-    -------------------
-    step1: prepare actions
-    step2: check_requirements action
-    step3: download files & copy on right location (hrd info is used)
-    step4: configure action
-    step5: check_uptime_local to see if process stops  (uses timeout $process.stop.timeout)
-    step5b: if check uptime was true will do stop action and retry the check_uptime_local check
-    step5c: if check uptime was true even after stop will do halt action and retry the check_uptime_local check
-    step6: use the info in the hrd to start the application
-    step7: do check_uptime_local to see if process starts
-    step7b: do monitor_local to see if package healthy installed & running
-    step7c: do monitor_remote to see if package healthy installed & running, but this time test is done from central location
-    """
 
-    def prepare(self,**args):
+    def build(self,**args):
         """
-        this gets executed before the files are downloaded & installed on approprate spots
         """
-        print 'TEST 1'
-        if not j.do.isDir('/opt/jumpscale7/include'):
-        	j.do.createDir('/opt/jumpscale7/include')
-        if not j.do.isDir('/opt/jumpscale7/share'):
-	        j.do.createDir('/opt/jumpscale7/share')
-        cmd = '''
-sudo apt-get install -y build-essential zlib1g-dev libyaml-dev libssl-dev libgdbm-dev libreadline-dev libncurses5-dev libffi-dev curl checkinstall libxml2-dev libxslt-dev libcurl4-openssl-dev libicu-dev logrotate python-docutils pkg-config cmake libkrb5-dev
-'''
-#        j.do.execute(cmd)
-        return True
         
-    # def configure(self,**args):
-    #     print 'TEST 2'
-    #     """
-    #     this gets executed when files are installed
-    #     this step is used to do configuration steps to the platform
-    #     after this step the system will try to start the jpackage if anything needs to be started
-    #     """
-    #     return True
 
-    # def stop(self,**args):
-    #     """
-    #     if you want a gracefull shutdown implement this method
-    #     a uptime check will be done afterwards (local)
-    #     return True if stop was ok, if not this step will have failed & halt will be executed.
-    #     """
-    #     return True
+        cmd = 'apt-get install -y bzip2 gcc build-essential zlib1g-dev libyaml-dev libssl-dev libgdbm-dev libreadline-dev libncurses5-dev libffi-dev curl checkinstall libxml2-dev libxslt-dev libcurl4-openssl-dev libicu-dev logrotate python-docutils pkg-config cmake libkrb5-dev'
+        j.action.start(retry=1, name="apt_get_install",description='', cmds=cmd, actionRecover=None, actionArgs={}, errorMessage='', die=True, stdOutput=True, jp=self.jp_instance)
+        
+        def downloadruby():
+            url='http://cache.ruby-lang.org/pub/ruby/2.2/ruby-2.2.0.tar.gz'
+            j.do.downloadExpandTarGz( url, destdir="/tmp/ruby", deleteDestFirst=True, deleteSourceAfter=True)
+        j.action.start(retry=1, name="ruby_installer",description='', action=downloadruby, actionRecover=None, actionArgs={}, errorMessage='', die=True, stdOutput=True, jp=self.jp_instance)
+        
+        cmds="""
+cd /tmp
+wget -O ruby-install-0.5.0.tar.gz https://github.com/postmodern/ruby-install/archive/v0.5.0.tar.gz
+tar -xzvf ruby-install-0.5.0.tar.gz
+cd ruby-install-0.5.0/
+sudo make install
+"""
+        j.action.start(retry=1, name="ruby_installer",description='', cmds=cmds, actionRecover=None, actionArgs={}, errorMessage='', die=True, stdOutput=True, jp=self.jp_instance)
 
-    # def halt(self,**args):
-    #     """
-    #     hard kill the app, std a linux kill is used, you can use this method to do something next to the std behaviour
-    #     """
-    #     return True
 
-    # def check_uptime_local(self,**args):
-    #     """
-    #     do checks to see if process(es) is (are) running.
-    #     this happens on system where process is
-    #     """
-    #     return True
+        cmds="ruby-install --install-dir /opt/ruby ruby"
+        j.action.start(retry=1, name="ruby_build",description='', cmds=cmds, actionRecover=None, actionArgs={}, errorMessage='', die=True, stdOutput=True, jp=self.jp_instance)
 
-    # def check_requirements(self,**args):
-    #     """
-    #     do checks if requirements are met to install this app
-    #     e.g. can we connect to database, is this the right platform, ...
-    #     """
-    #     return True
+        return True
 
-    # def monitor_local(self,**args):
-    #     """
-    #     do checks to see if all is ok locally to do with this package
-    #     this happens on system where process is
-    #     """
-    #     return True
 
-    # def monitor_remote(self,**args):
-    #     """
-    #     do checks to see if all is ok from remote to do with this package
-    #     this happens on system from which we install or monitor (unless if defined otherwise in hrd)
-    #     """
-    #     return True
-
-    # def cleanup(self,**args):
-    #     """
-    #     regular cleanup of env e.g. remove logfiles, ...
-    #     is just to keep the system healthy
-    #     """
-    #     return True
-
-    # def data_export(self,**args):
-    #     """
-    #     export data of app to a central location (configured in hrd under whatever chosen params)
-    #     return the location where to restore from (so that the restore action knows how to restore)
-    #     we remember in $name.export the backed up events (epoch,$id,$state,$location)  $state is OK or ERROR
-    #     """
-    #     return False
-
-    # def data_import(self,id,hrd,**args):
-    #     print 'TEST 3'
-    #     """
-    #     import data of app to local location
-    #     if specifies which retore to do, id corresponds with line item in the $name.export file
-    #     """
-    #     return True
-
-    # def uninstall(self,**args):
-    #     """
-    #     uninstall the apps, remove relevant files
-    #     """
-    #     pass
-
+    def package(self,**args):
+        """
+        """
+        src="/opt/ruby/"
+        dest="/opt/code/git/binary/ruby/ruby/"
+        j.system.fs.copyDirTree(src,dest,eraseDestination=True)
 
